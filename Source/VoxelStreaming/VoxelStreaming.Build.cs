bc299025d840ef2f9d4610c39f708f8e4494c0ce
Jaiminsinh Dodiya <jaiminsinh.gamedev@gmail.com>
2026-08-28 09:40:48 +0530

    VoxelStreaming: create ModuleRules Build.cs with module dependencies

---
+++ Source/VoxelStreaming/VoxelStreaming.Build.cs
@@ -0,0 +1,19 @@
+using UnrealBuildTool;
+
+public class VoxelStreaming : ModuleRules
+{
+	public VoxelStreaming(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"VoxelCore",
+			"VoxelRuntime",
+			"VoxelWorld",
+		});
+	}
+}
